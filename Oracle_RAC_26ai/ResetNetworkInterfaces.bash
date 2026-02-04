e145582e2cde80f5f367d964d454ae27965f9e8a
Sarma Pydipally <sarma1807@users.noreply.github.com>
2026-02-04 17:50:43 -0500

    Add files via upload

---
+++ Oracle_RAC_26ai/ResetNetworkInterfaces.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+############################
+# Author  : Sarma Pydipally
+# Created : 04-FEB-2026
+############################
+
+# check if script is being executed as root - if not exit with error
+if [[ ${EUID} -ne 0 ]]; then
+  echo "ERROR : This script must be executed as root user" >&2
+  exit 1
+fi
+
+rm -f /etc/udev/rules.d/70-custom-ifnames.rules
+cat /dev/null > /etc/hostname
+rm -f /etc/NetworkManager/system-connections/eth?.nmconnection
+
+echo "system will restart in 10 seconds ..."
+sleep 10
+shutdown -r now
+
+############################
+
